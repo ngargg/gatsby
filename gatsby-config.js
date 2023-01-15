627d3436879b588c8d2a265a17df773e6dd4f481
namanlp <naman@garudalinux.org>
2023-01-15 16:16:48 +0530

    Do some changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
+    siteMetadata: {
+    title: `Using Gatsby Head`,
+    description: `Example project for the Gatsby Head API`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/gatsby-icon.png`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
   plugins: [
     {
       resolve: "smooth-doc",
       options: {
-        name: "Smooth DOC Starter",
+        name: "Hello World",
         description: "Use your own description...",
         siteUrl: "https://example.com",
       },
